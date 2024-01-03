6912c34dc60cbe88e33eb25ab68ad204cdeec32f
dennislee22 <35444414+dennislee22@users.noreply.github.com>
2024-01-03 18:23:19 +0800

    Update run_tensorboard.py

--- run_tensorboard.py
+++ run_tensorboard.py
@@ -1,5 +1,4 @@
 import subprocess, sys, os
 CDSW_APP_PORT=os.environ['CDSW_APP_PORT']
-command = "tensorboard --load_fast=false --logdir trainlogs --host 127.0.0.1 --port $CDSW_APP_PORT"
+command = "tensorboard --load_fast=false --logdir trainoutput-wikisql --host 127.0.0.1 --port $CDSW_APP_PORT"
 subprocess.run(command, shell = True, executable="/bin/bash")
-
