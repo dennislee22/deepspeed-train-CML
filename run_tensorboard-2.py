c1cee63972f409552e098ad2f67fabc0c9235400
dennislee22 <35444414+dennislee22@users.noreply.github.com>
2024-01-03 18:23:08 +0800

    Delete run_tensorboard-2.py

--- run_tensorboard-2.py
+++
@@ -1,5 +0,0 @@
-import subprocess, sys, os
-CDSW_APP_PORT=os.environ['CDSW_APP_PORT']
-command = "tensorboard --load_fast=false --logdir trainoutput-wikisql/logs --host 127.0.0.1 --port $CDSW_APP_PORT"
-subprocess.run(command, shell = True, executable="/bin/bash")
-
